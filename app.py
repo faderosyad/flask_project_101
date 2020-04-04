5c67e0334feedb242f46d06136b77b71c6bd777d
fade.rosyad <fade.rosyad@advotics.com>
2020-04-05 01:19:22 +0700

    updating again

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import requests, json
 from flask import Flask, render_template, request, json, Response, jsonify
 from datetime import datetime, timedelta
 
+from resource.movie import movies
+
 from database.db import initialize_db
 from database.models import Movie
 
@@ -18,34 +20,7 @@ app.config['MONGIDB_SETTINGS'] = {
     }
 
 initialize_db(app)
-
-@app.route('/movies')
-def get_movies():
-    movies = Movie.objects().to_json()
-    return Response(movies, mimetype="application/json", status=200)
-
-@app.route('/movies', methods=['POST'])
-def add_movies():
-    body = request.get_json()
-    movie = Movie(**body).save()
-    id = movie.id
-    return {'id': str(id)}, 200
-
-@app.route('/movies/<id>', methods=['PUT'])
-def update_movie(id):
-    body = request.get_json()
-    Movie.objects.get(id = id).update(**body)
-    return '',200
-
-@app.route('/movies/<id>', methods=['DELETE'])
-def delete_movie(id):
-    Movie.objects.get(id = id).delete()
-    return '', 200
-
-@app.route('/movies/<id>')
-def get_movie(id):
-    movies = Movie.objects.get(id = id).to_json()
-    return Response(movies, mimetype="application/json", status=200)
+app.register_blueprint(movies)
 
 def getTimestamp():
     nowDateTime = datetime.now() + timedelta(hours=7)
