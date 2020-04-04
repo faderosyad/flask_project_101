9e754827eceb2cd8e8c8f4528dfb8018db2a7d2f
fade.rosyad <fade.rosyad@advotics.com>
2020-04-04 23:34:11 +0700

    first attempt implementing mongodb

--- app.py
+++ app.py
@@ -19,6 +19,35 @@ app.config['MONGIDB_SETTINGS'] = {
 
 initialize_db(app)
 
+@app.route('/movies')
+def get_movies():
+    movies = Movie.objects().to_json()
+    return Response(movies, mimetype="application/json", status=200)
+
+@app.route('/movies', methods=['POST'])
+def add_movies():
+    body = request.get_json()
+    movie = Movie(**body).save()
+    id = movie.id
+    return {'id': str(id)}, 200
+
+@app.route('/movies/<id>', methods=['PUT'])
+def update_movie(id):
+    body = request.get_json()
+    Movie.objects.get(id = id).update(**body)
+    return '',200
+
+@app.route('/movies/<id>', methods=['DELETE'])
+def delete_movie(id):
+    Movie.objects.get(id = id).delete()
+    return '', 200
+
+@app.route('/movies/<id>')
+def get_movie(id):
+    movies = Movies.objects.get(id = id).to_json()
+    return Response(movies, mimetype="application/json", status=200)
+
+
 def getTimestamp():
     nowDateTime = datetime.now() + timedelta(hours=7)
     formatDateTime = nowDateTime.strftime("%d-%m-%Y %H:%M:%S")
--- database/models.py
+++ database/models.py
@@ -1,6 +1,5 @@
 from .db import db
 
-
 class Movie(db.Document):
     name = db.StringField(required=True, unique=True)
     casts = db.ListField(db.StringField, required=True)
